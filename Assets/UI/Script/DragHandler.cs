4bbe56d890a30387c557a27981b29288721e3a70
MalletonJules <jules.malleton@brassart.fr>
2026-01-20 16:13:21 +0100

    <JM>_<Update>(UI drag script)(20/01)
    Je sais plus, je crois que je tente de rendre les éléments non interactible quand on va les casser.

--- Assets/UI/Script/DragHandler.cs
+++ Assets/UI/Script/DragHandler.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DragHandler : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class DragHandler : MonoBehaviour
     [SerializeField] private KeyCode _holdBreakMode = KeyCode.None;
     [Header("Settings")]
     [SerializeField] private DetachableUi _uiElement = null;
+    [SerializeField] private DetachableUi[] _detachableUis = null;
     [SerializeField] private float _shakeRate = 1.0f;
     [SerializeField] private float _maxDistanceBeforeBreaking = 20;
     [SerializeField] private float _maxToSnapBack = 10;
@@ -17,22 +20,30 @@ public class DragHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        _detachableUis = FindObjectsOfType<DetachableUi>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_uiElement != null)
+        if (Input.GetKey(_holdBreakMode))
         {
+            if (_uiElement.GetComponent<Selectable>() != null)
+                _uiElement.GetComponent<Selectable>().interactable = false;
             ShakeLogic();
         }
+        else
+            _uiElement.GetComponent<Selectable>().interactable = true;
     }
 
     private void ShakeLogic()
     {
-        if (Input.GetKey(_holdBreakMode) && _dragging)
+        if (_uiElement != null && _dragging)
         {
+
+            if (_uiElement.GetComponent<Selectable>() != null)
+                _uiElement.GetComponent<Selectable>().interactable = false;
+
             float distance = Vector2.Distance(_uiElement.InitialPosition, Input.mousePosition);
             if (_uiElement.Attached)
             {
