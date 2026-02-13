ea1e99406689c051dd04fe33cc0d3fcbe40d43fd
TheoARNAUD <63006868+TheoARNAUD@users.noreply.github.com>
2026-02-13 15:20:04 +0100

    <TA>-<Update>(door fixed + début sol)

--- Assets/GameObject/InterractableObject/Sol Piégé/PiegedFloor.cs
+++ Assets/GameObject/InterractableObject/Sol Piégé/PiegedFloor.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PiegedFloor : EnigmeObject
 {
+
+   
+
+   [SerializeField] private CharacterController _player;
+
+  [SerializeField]  private float _damage = 1;
+
+    private bool _inRange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,32 @@ public class PiegedFloor : EnigmeObject
     void Update()
     {
         
+    }
+
+
+   
+
+    public void TakeDamage()
+    {
+        if (_inRange)
+        {
+            _player.Hpdamage(_damage);
+
+           Invoke("TakeDamage", 1f);
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        _inRange = true;
+        TakeDamage(); 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _inRange = false;
+      
     }
 }
