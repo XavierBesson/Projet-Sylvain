8b867b798148a50f2ab69aa0de2c86566a8ffe6b
XavierBesson <156820625+XavierBesson@users.noreply.github.com>
2026-02-13 14:21:50 +0100

    <XB>_<Update>(Ennemy)(13/02)

--- Assets/GameObject/UIObject/UIObject.cs
+++ Assets/GameObject/UIObject/UIObject.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class UIObject : MonoBehaviour
 {
     [SerializeField] protected Rigidbody _rb;
-    [SerializeField] private float _dragDistance = 5;
+    [SerializeField] private float _dragDistance = 3;
     [SerializeField] private LayerMask _mask = 0;
     private bool _isDragging = false;
 
@@ -46,12 +46,8 @@ public class UIObject : MonoBehaviour
         {
             Vector3 mousePos = GameManager.Instance.Player.Camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, GameManager.Instance.Player.Camera.nearClipPlane + _dragDistance));
             _rb.MovePosition(mousePos);
-            _rb.rotation = Quaternion.Euler(45 * (Input.mousePosition.y - 540) / 540, 45 * (Input.mousePosition.x - 960) / 960, 0);
+            _rb.rotation = Quaternion.Euler(-(45 * (Input.mousePosition.y - 540) / 540), 70 * (Input.mousePosition.x - 960) / 960, 0);
         }
-
-
-        
-
     }
 
 
