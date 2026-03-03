e6dc6fe76df4e94598a9d5c06df56fc777fb9704
MalletonJules <127118689+JulesMalleton@users.noreply.github.com>
2026-03-03 18:34:05 +0100

    FIx enemy

--- Assets/GameObject/InterractableObject/Enemy/Enemy.cs
+++ Assets/GameObject/InterractableObject/Enemy/Enemy.cs
@@ -44,7 +44,7 @@ public class Enemy : EnigmeObject
     {
         PlaySound(_audioSource, _fuiteSound);
         GameManager.Instance.GameLoop += _followPath.ActivateFollowPath;
-        GameManager.Instance.GoodEnding = true;
+        GameManager.Instance.GoblinEnding = true;
         print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 
