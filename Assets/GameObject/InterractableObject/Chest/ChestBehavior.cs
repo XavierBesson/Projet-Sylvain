1ac8a8f4bdc7760a2a755d6378ed2d1bbdfd53cd
MalletonJules <127118689+JulesMalleton@users.noreply.github.com>
2026-03-13 11:34:15 +0100

    <JM>_<Update>(Chest + coins)(13/03)

--- Assets/GameObject/InterractableObject/Chest/ChestBehavior.cs
+++ Assets/GameObject/InterractableObject/Chest/ChestBehavior.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ChestBehavior : MonoBehaviour
 {
     [SerializeField] private Animator _chestAnimator;
     private bool _spin = false;
+    [Header("Explosion Settings")]
+    [SerializeField] private Vector3 _position;
+    [SerializeField] private float _radius = 0.5f;
+    [SerializeField] private float _force = 10f;
+    [SerializeField] private float _delay = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("Coinplosion", _delay);
     }
 
     // Update is called once per frame
@@ -29,5 +35,21 @@ public class ChestBehavior : MonoBehaviour
         }
     }
 
+    private void Coinplosion()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position + _position, _radius);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody rb = collider.GetComponent<Rigidbody>();
 
+            if (rb != null)
+                rb.AddExplosionForce(_force, transform.position + _position, _radius);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + _position, _radius);
+    }
 }
